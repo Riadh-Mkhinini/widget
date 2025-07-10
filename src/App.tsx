b7c709fe428d674a035c15fdd45b7b89c62c3412
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-10 14:35:30 +0100

    add vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => (
+  <div
+    style={{
+      border: "1px solid #ddd",
+      padding: "1em",
+      fontFamily: "sans-serif",
+    }}
+  >
+    <h3>📦 Bookini Widget</h3>
+    <p>Hello from embedded React!</p>
+  </div>
+);
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
