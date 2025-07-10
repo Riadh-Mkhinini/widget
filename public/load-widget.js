f40eeea28fe335b2373e550c04d6bc0d81c2b501
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-10 15:35:16 +0100

    fix

--- public/load-widget.js
+++ public/load-widget.js
@@ -2,10 +2,16 @@ import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom/client";
 
 import { initWidget } from "./widget.js";
-const scriptTag = document.currentScript;
+
+const scriptTag = Array.from(document.scripts).find((s) =>
+  s.src.includes("load-widget.js")
+);
 const container = document.getElementById("my-widget");
 
-if (container) {
+if (scriptTag && container) {
   const id = scriptTag.getAttribute("data-id");
-  initWidget(container, { id });
+
+  import("./widget.js").then(({ initWidget }) => {
+    initWidget(container, { id });
+  });
 }
