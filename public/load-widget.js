61666f3f3c9342f33e5e13556dfcb6825b53a939
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-10 15:32:11 +0100

    test id

--- public/load-widget.js
+++ public/load-widget.js
@@ -2,9 +2,10 @@ import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom/client";
 
 import { initWidget } from "./widget.js";
-
-const id = scriptTag.getAttribute("data-id");
+const scriptTag = document.currentScript;
 const container = document.getElementById("my-widget");
+
 if (container) {
+  const id = scriptTag.getAttribute("data-id");
   initWidget(container, { id });
 }
