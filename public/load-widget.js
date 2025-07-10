b9dd850d682a1ce760fb505d386bf777fb91b053
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-10 15:28:54 +0100

    remove id params

--- public/load-widget.js
+++ public/load-widget.js
@@ -3,7 +3,8 @@ import ReactDOM from "https://esm.sh/react-dom/client";
 
 import { initWidget } from "./widget.js";
 
+const id = scriptTag.getAttribute("data-id");
 const container = document.getElementById("my-widget");
 if (container) {
-  initWidget(container);
+  initWidget(container, { id });
 }
--- src/widget/index.tsx
+++ src/widget/index.tsx
@@ -1,21 +1,31 @@
 /* eslint-disable react-refresh/only-export-components */
+import type { FC } from "react";
 import ReactDOM from "react-dom/client";
 
-const Widget = () => (
-  <div
-    style={{
-      border: "1px solid #ddd",
-      padding: "1em",
-      fontFamily: "sans-serif",
-    }}
-  >
-    <h3>📦 Bookini Widget</h3>
-    <p>Hello from embedded React!</p>
-  </div>
-);
+const Widget: FC<Options> = (props) => {
+  const { id } = props;
+  return (
+    <div
+      style={{
+        border: "1px solid #ddd",
+        padding: "1em",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h3>📦 Bookini Widget</h3>
+      <p>Hello from embedded React!</p>
+      <p>
+        Loaded with ID: <strong>{id}</strong>
+      </p>
+    </div>
+  );
+};
 
+type Options = {
+  id?: string;
+};
 // This is the function that the host page will calls
-export function initWidget(container: HTMLElement) {
+export function initWidget(container: HTMLElement, options?: Options) {
   const root = ReactDOM.createRoot(container);
-  root.render(<Widget />);
+  root.render(<Widget id={options?.id} />);
 }
