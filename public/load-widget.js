d6cf016746628d8eeefde8e7ba07e8f9ab34d9f0
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-10 14:53:57 +0100

    fix error

--- public/load-widget.js
+++ public/load-widget.js
@@ -1,5 +1,6 @@
 import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom/client";
+
 import { initWidget } from "./widget.js";
 
 const container = document.getElementById("my-widget");
