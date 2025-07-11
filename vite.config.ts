0a1d6318f2a6b92f5b404ad8df478fba38540f3d
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-11 10:42:18 +0100

    new tets

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,36 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "inline-css",
+      apply: "build",
+      enforce: "post",
+      generateBundle(_, bundle) {
+        const jsFile = Object.keys(bundle).find((file) => file.endsWith(".js"));
+        const cssFile = Object.keys(bundle).find((file) =>
+          file.endsWith(".css")
+        );
+
+        if (jsFile && cssFile) {
+          const jsChunk = bundle[jsFile];
+          const cssChunk = bundle[cssFile];
+
+          if (jsChunk.type === "chunk" && cssChunk.type === "asset") {
+            jsChunk.code =
+              `const style = document.createElement('style');` +
+              `style.textContent = ${JSON.stringify(cssChunk.source)};` +
+              `document.head.appendChild(style);` +
+              jsChunk.code;
+
+            delete bundle[cssFile];
+          }
+        }
+      },
+    },
+  ],
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
