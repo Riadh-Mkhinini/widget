285de86c10604838a0f376285933937afd434c8c
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-11 10:39:19 +0100

    create new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Widget from "./widget/widget";
 
-const App = () => {
-  return <Widget options={{ id: "my-widget" }} />;
-};
+function App() {
+  return <Widget options={{ id: "widget" }} />;
+}
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from "url";
-
-// Fix for __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "inline-css-with-vars",
-      apply: "build",
-      enforce: "post",
-      generateBundle(_, bundle) {
-        const jsFile = Object.keys(bundle).find((f) => f.endsWith(".js"));
-        const cssFile = Object.keys(bundle).find((f) => f.endsWith(".css"));
-
-        if (jsFile && cssFile) {
-          const jsChunk = bundle[jsFile];
-          const cssChunk = bundle[cssFile];
-
-          if (jsChunk.type === "chunk" && cssChunk.type === "asset") {
-            const css = String(cssChunk.source);
-            const rootVars = css.match(/:root\s*\{[^}]+\}/)?.[0] ?? "";
-
-            jsChunk.code =
-              `
-const rootVars = document.createElement("style");
-rootVars.textContent = ${JSON.stringify(rootVars)};
-document.head.appendChild(rootVars);
-` +
-              `const style = document.createElement("style");
-style.textContent = ${JSON.stringify(css)};
-document.head.appendChild(style);
-` +
-              jsChunk.code;
-
-            delete bundle[cssFile];
-          }
-        }
-      },
-    },
-  ],
+  plugins: [react(), tailwindcss()],
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
   build: {
-    cssCodeSplit: false,
     lib: {
       entry: path.resolve(__dirname, "src/widget/widget.tsx"),
       name: "BookiniWidget",
@@ -59,7 +19,7 @@ document.head.appendChild(style);
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
