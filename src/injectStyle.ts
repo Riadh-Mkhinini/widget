ae22c72e045bf09904a3338652445ca998cfa117
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-14 11:26:23 +0100

    dsdsds

--- src/injectStyle.ts
+++ src/injectStyle.ts
@@ -2,7 +2,9 @@ export async function injectStyle(shadowRoot: ShadowRoot, cssUrl: string) {
   const res = await fetch(cssUrl);
   const cssText = await res.text();
   const style = document.createElement("style");
+
   style.setAttribute("type", "text/css");
   style.textContent = cssText;
+
   shadowRoot.appendChild(style);
 }
