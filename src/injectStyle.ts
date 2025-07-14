43480cd9a1965b02c72b10ac472d5fb019e70408
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-14 11:23:58 +0100

    test

--- src/injectStyle.ts
+++ src/injectStyle.ts
@@ -1,10 +1,8 @@
 export async function injectStyle(shadowRoot: ShadowRoot, cssUrl: string) {
   const res = await fetch(cssUrl);
   const cssText = await res.text();
-  const head = document.createElement("head");
   const style = document.createElement("style");
-  head.setAttribute("type", "text/css");
+  style.setAttribute("type", "text/css");
   style.textContent = cssText;
-  head.appendChild(style);
-  shadowRoot.appendChild(head);
+  shadowRoot.appendChild(style);
 }
